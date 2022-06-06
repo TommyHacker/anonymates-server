76832a9fe47ba6450aa9147be3b247db6f9ff83c
TommyHacker <t.hacker.webdev@gmail.com>
2022-06-06 14:20:30 +0100

    likes put route

--- articles_routes/articles.js
+++ articles_routes/articles.js
@@ -1,34 +1,71 @@
-const  express = require('express')
-const router = express.Router()
-const Article = require('../models/ArticleSchema')
+const express = require('express');
+const router = express.Router();
+const Article = require('../models/ArticleSchema');
 
+// create a new article POST request
+router.post('/:id', (req, res) => {
+	try {
+		//
+		const { title, body } = req.body;
+		if (!title || !body || body.length > 1000)
+			return res
+				.status(300)
+				.send(
+					'title and article body required. article body must be 1,000 characters or less.'
+				);
+		const article = new Article(title, body);
+		article.save();
+		res.json({
+			status: 'success',
+			message: 'article saved successfully.',
+			data: article,
+		});
+	} catch (err) {
+		console.log(err.message);
+		res.send('something went wrong while creating article.');
+	}
+});
+
+// comment on or like an article PUT request
+
+router.put('/:id/like', (req, res) => {
+	try {
+		const id = req.params.id;
+		const article = Article.findOne(id);
+		article.likes++;
+		article.save();
+		res.send(article);
+	} catch (err) {
+		console.log(err.message);
+		res.send('error');
+	}
+});
+
+router.put('/:id/comment', (req, res) => {
+    
+});
 
 //localhost:PORT/articles
-router.get('/', (req, res)=>{
-    try{
-        const articles =  Article.findAll()
-
-        res.send(articles)
-    } catch(error){
-        console.error(error)
-        res.send('something went wrong')
-    }
-})
-
+router.get('/', (req, res) => {
+	try {
+		const articles = Article.findAll();
+		res.send(articles);
+	} catch (error) {
+		console.error(error);
+		res.send('something went wrong');
+	}
+});
 
 //localhost:PORT/articles/:id
-router.get('/:id', (req, res)=>{
-    try{
-        const {id} = req.params
+router.get('/:id', (req, res) => {
+	try {
+		const { id } = req.params;
 
-        res.send(' single articles placed here')
-    } catch(error){
-        console.error(error)
-        res.send('something went wrong')
-    }
-})
+		res.send(' single articles placed here');
+	} catch (error) {
+		console.error(error);
+		res.send('something went wrong');
+	}
+});
 
-
-
-
-module.exports = router
+module.exports = router;
