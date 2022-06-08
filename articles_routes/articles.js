a598ad7b5dcff3a7c12333471c65cb82ec4a671c
tom harcker <thackerstudent@gmail.com>
2022-06-08 23:40:42 +0100

    changed likes error response to json

--- articles_routes/articles.js
+++ articles_routes/articles.js
@@ -55,7 +55,11 @@ router.post('/:id/like', (req, res) => {
 				likes: article.likes,
 			});
 		} else {
-			res.send('you cannot like an article twice.');
+			res.json({
+				status: 'failed',
+				message: 'you cannot like an article twice.',
+				data: article.likes,
+			});
 		}
 	} catch (err) {
 		console.log(err.message);
